2c52d7ce76aeeec96937b29c0d76eff4338f360e
unknown <yashkolnure58@gmail.com>
2026-04-14 19:11:15 +0530

    Initial commit

--- services/googleService.js
+++ services/googleService.js
@@ -3,18 +3,7 @@ const { google } = require('googleapis');
 const oauth2Client = new google.auth.OAuth2(
   process.env.GOOGLE_CLIENT_ID,
   process.env.GOOGLE_CLIENT_SECRET,
-<<<<<<< HEAD
   process.env.GOOGLE_REDIRECT_URI //
-);
-
-exports.getAuthUrl = (userId) => {
-  return oauth2Client.generateAuthUrl({
-    access_type: 'offline', // Gets refresh_token
-    prompt: 'consent',     // FORCES Google to send the refresh_token again
-    scope: ['https://www.googleapis.com/auth/business.manage'],
-    state: userId          // The ID we catch in the callback
-=======
-  process.env.GOOGLE_REDIRECT_URI // Should be http://localhost:5000/api/auth/google/callback
 );
 
 const getAuthUrl = (userId) => {
@@ -27,7 +16,6 @@ const getAuthUrl = (userId) => {
       'profile'
     ],
     state: userId // We pass the DB User ID to identify them in the callback
->>>>>>> d14f6c33b2c52dc1ef80db350caf70b49d704961
   });
 };
 
